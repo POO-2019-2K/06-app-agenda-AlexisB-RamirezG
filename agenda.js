45200b85ff052122750b1fc3c9a617a6484e14b0
Your Name <you@example.com>
2019-05-21 13:01:17 -0500

    Form validado

--- agenda.js
+++ agenda.js
@@ -1,3 +1,30 @@
 import Contact from "./contact.js";
 import List from "./list.js";
 
+class Agenda {
+    constructor() {
+        let list = new List();
+
+        document.querySelector("#btnAdd").addEventListener("click", () => {
+            let form = document.querySelector("#form");
+
+            if (form.checkValidity() === true) {
+                let name = document.querySelector("#name").value;
+                let birthday = document.querySelector("#date").value;
+                let email = document.querySelector("#email").value;
+
+                let objContact = {
+                    name: name,
+                    birthday: birthday,
+                    email: email
+                }
+
+                new Contact(objContact);
+            }
+
+            form.classList.add("was-validated");
+        });
+    }
+}
+
+new Agenda();
